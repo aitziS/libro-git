4f6ad0f2653a40d45ffd0a3a08ffffc7db21041b
aitziS <aitzisolba@gmail.com>
2023-11-18 12:04:03 +0000

    Otro mensaje de commit

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PRueba</title>
+    <title>Prueba</title>
 </head>
 <body>
     
+<p>
+    Capítulo 1. Introducción a Git
+    Capítulo 2. FLujo de trabajo básico
+    Capítulo 3. blablabla
+</p>
+
 </body>
 </html>
